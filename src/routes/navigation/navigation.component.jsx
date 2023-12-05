bfb14292f6807ed64bbccbf9dc297749a48695f6
mr-xero <husseinsobieh@yahoo.com>
2023-12-05 13:46:52 +0200

    part of nav done

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,71 @@
-import { NavigationContainer } from "./navigation.styles";
+import {
+  NavigationContainer,
+  TopBarContainer,
+  TopBarContentContainer,
+  SideBarContainer,
+  LogoContainer,
+  NavLinksContainer,
+  NavLink,
+  SupportContainer,
+  CartIcon,
+  AnalyticsIcon,
+  ProfileIcon,
+  SettingsIcon,
+  FaqIcon,
+  ChatIcon,
+  RightContentContainer,
+} from "./navigation.styles";
+import { ReactComponent as Logo } from "../../assests/logo.svg";
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <div></div>
+      <TopBarContainer>
+        <LogoContainer>
+          <Logo />
+        </LogoContainer>
+        <TopBarContentContainer>
+          <input type="search" placeholder="&#x1F50E;&#xFE0E; Search" />
+          <RightContentContainer>
+            <img alt="pic" src="../../assests/cart.svg" />
+            <div>
+              Hussein Sobieh
+              <img alt="pic" src="../../assests/logo.svg" />
+            </div>
+          </RightContentContainer>
+        </TopBarContentContainer>
+      </TopBarContainer>
+      <SideBarContainer>
+        <NavLinksContainer>
+          <NavLink to="/">
+            <AnalyticsIcon />
+            Dashboard
+          </NavLink>
+          <NavLink to="/orders">
+            <CartIcon />
+            Orders
+          </NavLink>
+          <NavLink to="/account">
+            <ProfileIcon />
+            Account
+          </NavLink>
+          <NavLink to="/settings">
+            <SettingsIcon />
+            Settings
+          </NavLink>
+        </NavLinksContainer>
+        <SupportContainer>
+          Support
+          <div>
+            <ChatIcon />
+            Chat
+          </div>
+          <div>
+            <FaqIcon />
+            FAQ
+          </div>
+        </SupportContainer>
+      </SideBarContainer>
     </NavigationContainer>
   );
 };
--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -1,6 +1,125 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { ReactComponent as CartSvg } from "../../assests/cart.svg";
+import { ReactComponent as ProfileSvg } from "../../assests/profile.svg";
+import { ReactComponent as AnalyticsSvg } from "../../assests/analytics.svg";
+import { ReactComponent as SettingsSvg } from "../../assests/setting.svg";
+import { ReactComponent as ChatSvg } from "../../assests/chat.svg";
+import { ReactComponent as FaqSvg } from "../../assests/faq.svg";
+
+const mainColor = "#eee";
+const subColor = "white";
 
 export const NavigationContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+export const TopBarContainer = styled.span`
+  width: 100%;
+  height: 70px;
   display: flex;
+  background-color: ${subColor};
+`;
+
+export const TopBarContentContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 80%;
+  input {
+    border-radius: 30px;
+    background-color: ${mainColor};
+    height: 40px;
+    width: 250px;
+    padding: 15px;
+    border: none;
+    margin-left: 50px;
+    display: block;
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 2px grey;
+    }
+  }
+`;
+
+export const RightContentContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const CartIcon = styled(CartSvg)``;
+export const AnalyticsIcon = styled(AnalyticsSvg)``;
+export const SettingsIcon = styled(SettingsSvg)``;
+export const ProfileIcon = styled(ProfileSvg)``;
+export const ChatIcon = styled(ChatSvg)``;
+export const FaqIcon = styled(FaqSvg)``;
+
+export const LogoContainer = styled.span`
+  width: 200px;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${mainColor};
+  svg {
+    height: 80%;
+  }
+`;
+
+export const SideBarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${subColor};
+  height: 100%;
+  width: 200px;
+`;
+
+export const NavLinksContainer = styled.div`
+  width: 100%;
+  height: 100%:
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const NavLink = styled(Link)`
+  width: 100%;
+  height: auto;
+  text-decoration: none;
+  color: black;
+  padding: 15px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  align-items: center;
+  svg {
+    width: 18px;
+    height: 18px;
+    padding: 0 10px 0 5px;
+  }
+`;
+
+export const SupportContainer = styled.div`
+  width: 100%;
+  height: auto;
+  color: #bbb;
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  flex-direction: column;
+  div {
+    padding-left: 10px;
+    padding-top: 10px;
+    color: black;
+    display: flex;
+    align-items: center;
+    svg {
+      width: 18px;
+      height: 18px;
+      padding: 0 10px 0 5px;
+    }
+  }
 `;
